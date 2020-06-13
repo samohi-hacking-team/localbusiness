8bbeda0fc19c975fdf469faecaddb82eee42af80
michellehwin <michellehwin@gmail.com>
2020-06-13 14:46:55 -0500

    create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+//Etsy BLM Integration
+
+//check against etsyBLM.json
+if (true) {
+    $(".listing-link").parent().hide();
+}
+window.onload = function() {
+    document.write('Hello world');
+    this.console.log("hello michelle");
+}
